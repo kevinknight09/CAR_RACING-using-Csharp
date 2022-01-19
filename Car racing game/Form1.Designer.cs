5dfdddfd6ee0add29e70615fb147a3d9a682dea8
Rounak Saha <sahakrishna85@gmail.com>
2022-01-19 22:07:13 +0530

    Add project files.

---
+++ Car racing game/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Car_racing_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.car = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.obstacle3 = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(4)))), ((int)(((byte)(51)))));
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // car
+            // 
+            resources.ApplyResources(this.car, "car");
+            this.car.Name = "car";
+            this.car.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            resources.ApplyResources(this.obstacle1, "obstacle1");
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.TabStop = false;
+            // 
+            // obstacle2
+            // 
+            resources.ApplyResources(this.obstacle2, "obstacle2");
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.TabStop = false;
+            // 
+            // obstacle3
+            // 
+            resources.ApplyResources(this.obstacle3, "obstacle3");
+            this.obstacle3.Name = "obstacle3";
+            this.obstacle3.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.obstacle3);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox car;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.PictureBox obstacle3;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Car racing game/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_racing_game
+{
+    public partial class Form1 : Form
+    {
+        int carSpeed = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            obstacle1.Top += 2;
+            obstacle2.Top += 2;
+            obstacle3.Top += 2;
+
+
+            if(car.Top <=0 || car.Top>= 470)
+            {
+                gameTimer.Stop();
+            }
+            
+                if (carSpeed > 0)
+                {
+                    car.Top -= carSpeed;
+                }
+                else if (carSpeed <= 0)
+                {
+                    car.Top -= carSpeed;
+                }
+         
+            
+            if(obstacle1.Top>=550)
+            {
+                obstacle1.Top = 0;
+            }
+            if (obstacle2.Top >= 550)
+            {
+                obstacle2.Top = 0;
+            }
+            if (obstacle3.Top >= 550)
+            {
+                obstacle3.Top = 0;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                if(car.Top > 0)
+                {
+                    if(carSpeed<2)
+                    {
+                        carSpeed += 1;
+                    }
+
+                   // car.Top -= carSpeed;
+                }
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                if(car.Left >200)
+                {
+                    car.Left -=4;
+                }
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                if(car.Right <650)
+                {
+                    car.Left += 4;
+                }
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                if(car.Top < 470)
+                {
+                    if(carSpeed > -1)
+                    {
+                        carSpeed -= 1;
+                    }
+                    
+                   // car.Top += carSpeed ;
+                }
+            }
+        }
+    }
+}
